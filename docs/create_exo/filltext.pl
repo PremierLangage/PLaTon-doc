b51b6d92e93cfcdf203dbfdd4be7beb87d024cf5
ddoyen <38214648+ddoyen@users.noreply.github.com>
2021-06-17 00:21:52 +0200

    Create filltext.pl

---
+++ docs/create_exo/filltext.pl
@@ -0,0 +1,8 @@
+# Texte à compléter
+
+Le modèle `basic/sortlist` permet de fabriquer des exercices où il faut compléter un texte avec des étiquettes. 
+
+Les clés de base de ce modèle sont :
+
+  * `question` : l'énoncé de l'exercice ;
+  * `filledtext` : le texte complété.
